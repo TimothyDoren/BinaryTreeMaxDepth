283c86401d700cfb7a5d7b890dcd727c68ff8e4f
Tim D <124634979+TimothyDoren@users.noreply.github.com>
2023-11-26 15:51:58 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using BinaryTreeMaxDepth;
+
+// Step 1: Create individual nodes
+TreeNode root = new TreeNode(3);
+TreeNode node9 = new TreeNode(9); // This node has no children
+TreeNode node20 = new TreeNode(20);
+TreeNode node15 = new TreeNode(15); // This node has no children
+TreeNode node7 = new TreeNode(7);   // This node has no children
+
+// Step 2: Link the nodes to form the tree
+root.left = node9;
+root.right = node20;
+node20.left = node15;
+node20.right = node7;
+
+Console.WriteLine(root.MaxDepth(root));
---
+++ TreeNode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeMaxDepth
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        public int MaxDepth(TreeNode root)
+        {
+            int depth = 0;
+            if (root == null)
+            {
+                depth = 0;
+            }
+            if (root != null)
+            {
+                int leftDepth = MaxDepth(root.left);
+                int rightDepth = MaxDepth(root.right);
+
+                depth = Math.Max(leftDepth, rightDepth) + 1;
+            }
+
+            return depth;
+        }
+    }
+}
